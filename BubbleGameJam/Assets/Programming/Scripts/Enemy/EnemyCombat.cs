f34955277b95d304f1ecd517ea5f07d9c955b67a
FrostHive <54725116+FrostHive@users.noreply.github.com>
2025-01-23 13:30:37 -0600

    Added basic combat script
    Added PlayerCombat.cs for starting logic on combat. Edited EnemyCombat.cs as the enemies will not have health, but rather be taken out in one shot when stepped on.

--- BubbleGameJam/Assets/Programming/Scripts/Enemy/EnemyCombat.cs
+++ BubbleGameJam/Assets/Programming/Scripts/Enemy/EnemyCombat.cs
@@ -5,19 +5,20 @@ public class EnemyCombat : MonoBehaviour
 {
     //Still to write
     //- Logic to trap the enemy in bubble
-    //- How the enemy damages the player
     //- Navigation towards the player
     //
     //Some of these can and should be put into separate scripts
 
-    public int health;
-    public int damage;
+    private bool trappedInBubble;
+    public bool steppedOn;
 
-    public bool trappedInBubble;
+    private void Start()
+    {
+    }
 
     private void Update()
     {
-        if (health <= 0)
+        if (steppedOn)
         {
             StartCoroutine(Death());
         }
@@ -30,4 +31,20 @@ public class EnemyCombat : MonoBehaviour
 
         Destroy(gameObject);
     }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        GameObject collider = collision.gameObject;
+        if (collider.tag == "Bubble")
+        {
+            //Implement TrappedInBubble here when bubble mechanic is created
+            trappedInBubble = true;
+        }
+    }
+    public IEnumerator TrappedInBubble()
+    {
+        //Play Bubble-trapping animation
+        yield return null;
+    }
+
 }
