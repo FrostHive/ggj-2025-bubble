2ffdb48dc57bffc0714efde7dd24e2567b991cb6
FrostHive <54725116+FrostHive@users.noreply.github.com>
2025-01-26 15:43:41 -0600

    More work on boss animation

--- BubbleGameJam/Assets/Programming/Scripts/Enemy/BossAnimator.cs
+++ BubbleGameJam/Assets/Programming/Scripts/Enemy/BossAnimator.cs
@@ -2,6 +2,8 @@ using UnityEngine;
 
 public class BossAnimator : MonoBehaviour
 {
+    [SerializeField] private Animator animator;
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
